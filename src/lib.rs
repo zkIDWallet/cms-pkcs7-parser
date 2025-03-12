8f5af38db0a1a943f6cc80eed6687af0926873e9
paolo9921 <71081956+paolo9921@users.noreply.github.com>
2025-03-12 16:22:20 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 pub mod pkcs7;
 
 pub use pkcs7::loader::load_pkcs7;
-pub use pkcs7::types::Pkcs7;
+pub use pkcs7::types::{
+    Pkcs7,
+    SignedData,
+    Certificate,
+    CertificateData,
+    CrlData,
+    PublicKey,
+};
 /*
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
